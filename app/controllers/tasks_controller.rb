552f1f451c56ceec3ac1aaea2819e51ba14951ce
STEPHANE POLETTI <polettistephane@msn.com>
2021-05-25 15:24:05 +0200

    show ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,13 @@
 class TasksController < ApplicationController
   def show
-    @task = Task.find(params :id)
+    @task = Task.find(params[:id])
   end
 
   def index
     @tasks = Task.all
   end
 
-
-
+  def new
+    @restaurant = Restaurant.new
+  end
 end
-
