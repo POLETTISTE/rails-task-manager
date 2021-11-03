a12b7118f1b8863304f42e636722fee537a2ac41
STEPHANE POLETTI <polettistephane@msn.com>
2021-11-03 16:56:25 +0100

    add deadline to task table

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,7 +23,7 @@ class TasksController < ApplicationController
 
   def update
     @task.update(task_params)
-    redirect_to task_path(@task)
+    redirect_to tasks_path
   end
 
   def destroy
@@ -38,6 +38,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :details, :completed)
+    params.require(:task).permit(:title, :details, :deadline, :completed)
   end
 end
